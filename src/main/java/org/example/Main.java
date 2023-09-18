5e7c2ad69fb017b7fca70733db5e6e6f2b44c49e
ckh <gusrudchl12@gmail.com>
2023-09-18 10:54:45 +0900

    test_4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,7 @@ public class Main {
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 4; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
+
             System.out.println("i = " + i);
         }
     }
