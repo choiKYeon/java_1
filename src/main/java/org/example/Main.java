50a1e93c7eec799af6dde8fd9b4e1b1ed6cb87f5
ckh <gusrudchl12@gmail.com>
2023-09-18 11:10:01 +0900

    assertj

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 4; i++) {
-
 
             System.out.println("i = " + i);
         }
